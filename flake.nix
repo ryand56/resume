c41369427dfb73fab5fbf33d84f442a67d68050d
Ryan Omasta <git@ryand.ca>
2026-05-25 21:44:51 -0600

    initial

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "Resume + Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    forAllSystems = f:
+      let
+        lib = nixpkgs.lib;
+
+        genAttrs = lib.genAttrs;
+        intersectLists = lib.intersectLists;
+
+        platforms = lib.platforms;
+      in
+      genAttrs (
+        intersectLists platforms.linux (
+          platforms.x86_64 ++ platforms.aarch64
+        )
+      ) (system: f nixpkgs.legacyPackages.${system});
+  in {
+    packages = forAllSystems (pkgs: {
+      resume =
+        pkgs.runCommand "resume.pdf" {
+          src = pkgs.lib.cleanSourceWith {
+            src = self;
+            filter = path: type:
+              let
+                base = baseNameOf path;
+              in
+                base == "resume.md" ||
+                base == "meta.yml" ||
+                base == "layout.tex";
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pandoc
+            (texliveSmall.withPackages (ps: with ps; [
+              geometry
+              titlesec
+              hyperref
+              enumitem
+              parskip
+              sectsty
+            ]))
+          ];
+        }
+        ''
+          set -eu
+          pandoc "$src/resume.md" \
+            -o "$out" \
+            --metadata-file="$src/meta.yml" \
+            --pdf-engine=xelatex \
+            --template="$src/layout.tex"
+        '';
+
+      default = self.packages.resume;
+    });
+  };
+}
